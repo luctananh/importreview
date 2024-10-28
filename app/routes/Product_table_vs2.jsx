fc5d5cbb7be6d0d6ebe3c144db32c8ae95a6224e
trancuong612 <tranvcuong612@gmail.com>
2024-10-28 13:48:36 +0700

    edit file

--- app/routes/Product_table_vs2.jsx
+++ app/routes/Product_table_vs2.jsx
@@ -222,7 +222,7 @@ export default function ProductTable() {
                 onClick={() =>
                   (window.location.href = `/product_reviews/?pI=${
                     product.id
-                  }&na=${encodeURIComponent(product.name)}`)
+                  }&na=${encodeURIComponent(product.name)}&order=asc`)
                 }
               >
                 <td>
