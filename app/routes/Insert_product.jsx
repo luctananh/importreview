5e07f8cb66405e68c4e696236458d3e902f14664
trancuong612 <tranvcuong612@gmail.com>
2024-10-29 10:47:43 +0700

    edit file

--- app/routes/Insert_product.jsx
+++ app/routes/Insert_product.jsx
@@ -230,7 +230,7 @@ export default function ProductTable() {
         </div>
       </div>
 
-      <section className="bg-white">
+      {/* <section className="bg-white">
         <div className="max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
           <nav className="flex flex-wrap justify-center -mx-5 -my-2">
             <div className="px-5 py-2">
@@ -359,7 +359,7 @@ export default function ProductTable() {
             © 2023 Import Review Aliexpress.
           </p>
         </div>
-      </section>
+      </section> */}
     </>
   );
 }
