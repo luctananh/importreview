b7a241da579df18628d135b82e72a96db12f262a
trancuong612 <tranvcuong612@gmail.com>
2024-10-30 16:39:06 +0700

    edit file

--- app/routes/logout.jsx
+++ app/routes/logout.jsx
@@ -1,9 +1,9 @@
-import Home300 from "./product_table";
-export default function () {
-  return (
-    <>
-      <h1>Logout thanh cong</h1>
-      <Home300 />
-    </>
-  );
-}
+// // import Home300 from "./product_table";
+// export default function () {
+//   return (
+//     <>
+//       <h1>Logout thanh cong</h1>
+//       <Home300 />
+//     </>
+//   );
+// }
